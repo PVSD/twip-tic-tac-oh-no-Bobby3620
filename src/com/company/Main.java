e8dd8d89366c225cffdc71f33043f33706cf43fa
rd568 <bobbyday3620@gmail.com>
2018-12-17 11:21:36 -0500

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //The user will choose the game mode
+        System.out.println("Welcome user! You're going to play a game of tic tac toe.");
+        System.out.println("You have three options. Choose 1 for user vs user. Choose 2 for user vs computer. Choose 3 for computer vs computer(1, 2, 3).");
+
+        //Scanner for the user to choose
+        Scanner kbInput = new Scanner(System.in);
+        int option = kbInput.nextInt();
+
+        //If 1 is selected
+        if(option == 1){
+            System.out.println("You selected user vs user");
+
+
+        }
+
+        //If 2 is selected
+        if(option == 2){
+            System.out.println("You have selected user vs computer");
+
+        }
+
+        //If 3 is selected
+        if(option == 3){
+            System.out.println("You have selected computer vs computer");
+
+        }
+
+
+
     }
 }
---
+++ src/com/company/UserVsUser.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by rd568 on 12/17/18.
+ */
+public class UserVsUser {
+
+    //The array is the board where [0][] is the row, [][0][] is the column, [][0] is the spot in the row + column
+    public int[][] board = new int[][] { {1, 2, 3}, {1, 2, 3} };
+
+}
